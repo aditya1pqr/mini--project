46dd94b6d5d28465e277924170916a5f3f7c6f5d
Aditya <aadi099012@gmail.com>
2024-08-11 22:46:51 +0530

    adding calculator components

---
+++ project_12_calculator/calculator/src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Display() {
+  return (
+    <div>
+        <input  type="text" name="" id="" />
+      
+    </div>
+  )
+}
+
+export default Display
---
+++ project_12_calculator/calculator/src/components/DisplayButtons.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+function DisplayButtons() {
+  return (
+    <div className='sub-conatiner'>
+      <Button></Button>
+      
+      <Button></Button>
+      
+      <Button></Button>
+
+
+
+    </div>
+  )
+}
+
+export default DisplayButtons
