f24a0f62ed86d78b6f0a63870c4dd9392567a06c
Aditya <aadi099012@gmail.com>
2024-09-17 16:16:52 +0530

    theme changer  completed

--- project_15_themechanger/themechanger/src/App.jsx
+++ project_15_themechanger/themechanger/src/App.jsx
@@ -1,13 +1,29 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 import Btn from './components/btn'
 import Card from './components/Card'
+import { ThemeProvider } from './context/theme'
 
 function App() {
+  const [themeMode , setThemeMode] = useState('light');
 
+  const darktheme=()=>{
+    setThemeMode('dark');
+
+  }
+  const  lighttheme=()=>{
+    setThemeMode('light')
+  }
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light" , "dark")
+    document.querySelector('html').classList.add(themeMode)
+
+   
+  },[themeMode])
 
   return (
-    <>
+    <ThemeProvider value={{themeMode, darktheme, lighttheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -19,7 +35,7 @@ function App() {
               </div>
           </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
